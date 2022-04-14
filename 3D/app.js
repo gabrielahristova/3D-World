ea02ebe1dc86b4a07389bca6e0f847061de6c361
Gabriela Hristova <94860950+gabrielahristova@users.noreply.github.com>
2022-04-14 22:19:32 +0300

    Add files via upload

---
+++ 3D/app.js
@@ -0,0 +1,43 @@
+function main() {
+
+    const canvas = document.querySelector('#canvas');
+    const fov = 50;
+    const aspect = canvas.clientWidth / canvas.clientHeight;
+    const near = 0.1;
+    const far = 2000;
+
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 1;
+
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
+    
+
+    new THREE.OrbitControls(camera, canvas);
+
+    const scene = new THREE.Scene();
+    const loader = new THREE.TextureLoader();
+
+    const texture = loader.load(
+        "https://threejs.org/manual/examples/resources/images/equirectangularmaps/tears_of_steel_bridge_2k.jpg",
+        () => {
+            const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
+            rt.fromEquirectangularTexture(renderer, texture);
+            scene.background = rt.texture;
+        }
+    )
+
+    function render() {
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+        
+        renderer.setSize(width, height, false);
+        renderer.render(scene, camera);
+        requestAnimationFrame(render)
+    }
+    requestAnimationFrame(render)
+}
+
+main();
